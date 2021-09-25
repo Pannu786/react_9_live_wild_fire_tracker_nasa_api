a0bc3ea3c0a53bdb61e98bd926dc401595b8f6be
Pannu786 <thepannu786@gmail.com>
2021-09-25 13:58:31 +0530

    all dne ✔️

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,7 +1,15 @@
-import React from 'react';
-
-const LocationInfoBox = () => {
-  return <div></div>;
+const LocationInfoBox = ({ info }) => {
+  return (
+    <div className='location-Info'>
+      <h2>Event Location Info</h2>
+      <ul>
+        <li>
+          ID: <strong>{info.id}</strong>
+          TITLE: <strong>{info.title}</strong>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default LocationInfoBox;
